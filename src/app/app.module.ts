dea9170cfb1f75462ed501d87c6379f269889918
aalsabag <aalsabag@AAlsabag.localdomain>
2020-10-29 23:59:24 -0500

    Create nodelement component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NodeElementComponent } from './node-element/node-element.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NodeElementComponent
   ],
   imports: [
     BrowserModule
